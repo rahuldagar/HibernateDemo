dc635bccbbeae4ee46a60ab8aa8c0dddfd9c68d4
suhas1294 <suhasr94@gmail.com>
2019-06-17 21:49:27 +0530

    changes for deprecated method buildSessionFactory

--- src/main/java/com/suhas/App.java
+++ src/main/java/com/suhas/App.java
@@ -1,8 +1,11 @@
 package com.suhas;
 
+import javax.imageio.spi.ServiceRegistry;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -26,8 +29,13 @@ public class App
         // .configure() to make use of 'hibernate.cfg.xml', .addAnnotatedClass() to specify which entity you are working with
         // Exception: No identifier specified for entity: com.suhas.Alien : No primary key mentioned in alien class
         Configuration configuration = new Configuration().configure().addAnnotatedClass(Alien.class);
+        
         SessionFactory sf = configuration.buildSessionFactory();
         Session session = sf.openSession();
+        
+        // since 'buildSessionFactory' is deprecated, use this:
+        // ServiceRegistry reg = new ServiceRegistryBuilder().apply(configuration.getProperties()).buildServiceRegistry();
+        // SessionFactory sf = configuration.buildSessionFactory(reg);
         
         // to save data, u need to follow ACID properties : Atomicity, Consistency, Isolation & Durability
         Transaction tx = session.beginTransaction();
