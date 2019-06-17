7447e57de57045fe13942de67e4dbfd13e1bb9d1
suhas1294 <suhasr94@gmail.com>
2019-06-17 22:05:10 +0530

    usage of various annotations

--- src/main/java/com/suhas/Alien.java
+++ src/main/java/com/suhas/Alien.java
@@ -1,13 +1,33 @@
 package com.suhas;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
+// @Entity(name="alien_table") if you want your database table name to be "alien_table", 
+// also this should be supported by <property name="hbm2ddl.auto">create</property> in hibernate.cfg.xml 
+
+/*
+ Three layers of entity are there:
+ 1. class Name
+ 2. Entity name
+ 3. Table name
+ 
+ If we want to change only table name, 
+ @Table(name="alien_table")
+ 
+ Table and Entity both are different
+ */
 @Entity
 public class Alien {
 	@Id
 	private int Alienid;
+	// @Transient
+	// means, name will not be getting stored in database, there will be no column in db
 	private String name;
+	//@Column(name = "alien_tech")
+	// alien_tech will be column in database
 	private String tech;
 	
 	@Override
