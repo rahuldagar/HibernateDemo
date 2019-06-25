18a48c65fc906d8b79357233fc7de9b3886880bd
suhas1294 <suhasr94@gmail.com>
2019-06-25 21:23:57 +0530

    hibernate caching 2 : limitation of first level cache

--- src/main/java/com/suhas/CachingDemo.java
+++ src/main/java/com/suhas/CachingDemo.java
@@ -7,23 +7,23 @@ import org.hibernate.cfg.Configuration;
 
 public class CachingDemo {
 	public static void main(String args[]) {
-		Alien alien = null;
+		Alien alien1 = null;
+		Alien alien2 = null;
 		Configuration configuration = new Configuration().configure().addAnnotatedClass(Alien.class).addAnnotatedClass(Laptop.class);
     SessionFactory sf = configuration.buildSessionFactory();
+    
     Session session1 = sf.openSession();
     session1.beginTransaction();
-    
-    alien = (Alien) session1.get(Alien.class, 101);
-    System.out.println(alien);
-
-    alien = (Alien) session1.get(Alien.class, 101);
-    System.out.println(alien);
-
-    // Notice that only one query is done in logs
-    // when above two queries are executed, hibernate first checks whether the value is
-    // there in first level and second level cache, if its not there, then it will fetch from db
-
+    alien1 = (Alien) session1.get(Alien.class, 101);
+    System.out.println(alien1);
     session1.getTransaction().commit();
+
+    Session session2 = sf.openSession();
+    session2.beginTransaction();
+    alien2 = (Alien) session2.get(Alien.class, 101);
+    System.out.println(alien2);
+    session2.getTransaction().commit();
     
+    // Now observer that two queries are fired for two different transactions
 	}
 }
